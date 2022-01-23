507f0e0d39d4bb4570c9b79a5f5f6c8c20411c5c
Kaique Gimenez <kaiquesgimenez_sg_@hotmail.com>
2022-01-23 11:21:10 -0300

    Adicionando o arquivo Procfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://yntoaqkpvnmnqt:b3c5924bb951be3d29d54a477f7d68f2bb74347ea80cc51b3a227d9bee7221b8@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d4jmitrdkgq9h4',
-    ssl: { rejectUnauthorized: false },
+    ssl: true,
     migrations : {
       tableName: 'knex_migrations',
       directory: `${__dirname}/api/database/migrations`
